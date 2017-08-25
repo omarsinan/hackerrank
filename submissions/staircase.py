e2a48df9f714c6f47f379e673fa027fec74bbae5
Omar Sinan <omaro.hashim@hotmail.com>
2017-08-26 01:11:55 +0300

    Upload staircase.py

---
+++ submissions/staircase.py
@@ -0,0 +1,18 @@
+#!/bin/python
+
+import sys
+
+
+n = int(raw_input().strip())
+
+spaces = n - 1
+hashes = 1
+
+for i in range(n):
+    for j in range(spaces):
+        sys.stdout.write(' ')
+    for k in range(hashes):
+        sys.stdout.write('#')
+    hashes += 1
+    spaces -= 1
+    print
