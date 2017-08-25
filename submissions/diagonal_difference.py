2823c23779fac78a42b59a562d3c603e6384fe0a
Omar Sinan <omaro.hashim@hotmail.com>
2017-08-26 01:09:51 +0300

    Upload diagonal_difference.py

---
+++ submissions/diagonal_difference.py
@@ -0,0 +1,16 @@
+#!/bin/python
+
+import sys
+
+
+n = int(raw_input().strip())
+a = []
+for a_i in xrange(n):
+    a_temp = map(int,raw_input().strip().split(' '))
+    a.append(a_temp)
+
+sum = 0
+for i in range(n):
+    sum += a[i][i] - a[i][len(a[i]) - 1 - i]
+    
+print abs(sum)
