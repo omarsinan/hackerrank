f342161eb0cf3f9596903a6f6fd2c5a5c6f68f8e
Omar Sinan <omaro.hashim@hotmail.com>
2017-08-26 18:40:34 +0300

    Create merge_the_tools.py

---
+++ submissions/merge_the_tools.py
@@ -0,0 +1,21 @@
+# author: Omar Sinan
+# date: 26/08/2017
+# description: HackerRank's "Merge the Tools!" Coding Challenge
+
+def merge_the_tools(string, k):
+    n = len(string)/k
+    segs = []
+    
+    for i in range(0, len(string), k):
+        segs.append(string[i : i + k])
+        
+    for seg in segs:
+        occurances = []
+        for i in seg:
+            if i not in occurances:
+                occurances.append(i)
+        print ''.join(occurances)
+    
+if __name__ == '__main__':
+    string, k = raw_input(), int(raw_input())
+    merge_the_tools(string, k)
