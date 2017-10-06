9d216ec51030260cd564f884409727a5c89710a2
Omar Sinan <omaro.hashim@hotmail.com>
2017-10-06 18:42:31 +0300

    Update the_minnion_game.py

--- submissions/the_minnion_game.py
+++ submissions/the_minnion_game.py
@@ -1,21 +1,20 @@
 # author: Omar Sinan
 # date: 26/08/2017
+# updated: 06/10/2017 - After 2 months of python, I decided to revist this challenge
 # description: HackerRank's "The Minnion Game" Coding Challenge
 
 def minion_game(string):
-    k = s = 0 #kevin: vowels
-    for i, val in enumerate(string):
-        if val in 'AEIOU':
-            k += len(string) - i
+        k = s = 0 #kevin: vowels
+        
+        k = sum([len(string) - i for i in range(len(string)) if string[i] in 'AEIOU'])
+        s = sum([len(string) - i for i in range(len(string)) if string[i] not in 'AEIOU'])
+        
+        if s > k:
+            print "Stuart", s
+        elif k > s:
+            print "Kevin", k
         else:
-            s += len(string) - i
-
-    if s > k:
-        print "Stuart", s
-    elif k > s:
-        print "Kevin", k
-    else:
-        print "Draw"
+            print "Draw"
        
 if __name__ == '__main__':
     s = raw_input()
